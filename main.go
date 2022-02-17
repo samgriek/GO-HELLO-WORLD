aeca2a8ae50127256b41ee60bf90330488747a83
samgriek <75908439+samgriek@users.noreply.github.com>
2022-02-17 17:22:18 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"myapp/doctor"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	whatToSay := doctor.Intro()
+
+	fmt.Println(whatToSay)
+
+	for {
+		userInput, _ := reader.ReadString('\n')
+		response := doctor.Response(userInput)
+		fmt.Println(response)
+	}
+}
