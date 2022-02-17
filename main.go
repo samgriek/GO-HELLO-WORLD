a40b6f60683d609180b5da013916f1e2dc3061b1
Samuel Griek <samuel.griek@nielsen.com>
2022-02-17 19:59:15 +0100

    add quit

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"myapp/doctor"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -16,7 +17,16 @@ func main() {
 
 	for {
 		userInput, _ := reader.ReadString('\n')
-		response := doctor.Response(userInput)
-		fmt.Println(response)
+
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		if userInput == "quit" {
+			break
+		} else {
+			response := doctor.Response(userInput)
+			fmt.Println(response)
+		}
+
 	}
 }
